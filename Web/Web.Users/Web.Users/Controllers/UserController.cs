2f28446a0fc65e04c7c86b3bdaf0e918626b2941
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-01-14 12:30:38 -0600

    Challenge Done

---
+++ Web/Web.Users/Web.Users/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Users.Models;
+
+namespace Web.Users.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> list = new List<User>();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(list);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                list.Add(user);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Web/Web.Users/Web.Users/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Users.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(30)]
+        public String FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public String LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        public String Phone { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public String Address { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public String Company { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public String Role { get; set; }
+    }
+}
